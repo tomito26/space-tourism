fa971a6e411772f73e9d7f94b3d335a64fc5847f
tomito26 <tommybwah@gmail.com>
2022-05-25 18:21:20 +0300

    added home component and also styled the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
-import logo from 'assets'
+import logo from '../logo.svg'
 const  Navbar = () => {
     return(
-        <nav>
-            <div className="logo">
-                <img src={logo} alt="" />
-            </div>
-        </nav>
+        <div className="navbar">
+            <nav>
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className='line'></div>
+                <ul>
+                    <li><a className='navlink' href="#home"><span>00</span><span>Home</span></a></li>
+                    <li><a className='navlink' href="#destination"><span>01</span><span>Destination</span></a></li>
+                    <li><a className='navlink' href="#crew"><span>02</span><span>Crew</span></a></li>
+                    <li><a className='navlink' href="#Technology"><span>03</span><span>Technology</span></a></li>
+                </ul>
+            </nav>
+        </div>
     );
 };
 export default Navbar;
