a277074050b51bbf83a2711e05950d98d0172f28
tomito26 <tommybwah@gmail.com>
2022-05-26 23:15:38 +0300

    added a description on the home page that gives more information about this website and styled the description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 const Home = () =>{
     return(
         <div className="home">
-            
+            <div className="overlay-text">
+                <h5>So, you want to travel to </h5>
+                <h2>space</h2>
+                <p>Lets face it; if want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience</p>
+            </div>
+            <div className="explore">
+                <p>Explore</p>
+            </div>
         </div>
     );
 };
