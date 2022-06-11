403b8b258895940941919dd15499419a3cbc8fd2
tomito26 <tommybwah@gmail.com>
2022-06-11 14:07:24 +0300

    added the crew data to an array on crewData which i then import it in the crew page and map through it and created a slider  which display the crew info once the slider buttons are clicked

---
+++ src/components/CrewData.js
@@ -0,0 +1,31 @@
+import commander from '../assets/crew1.png';
+import mission_specialist from '../assets/crew2.png';
+import pilot from '../assets/crew3.png';
+import  flight_engineer from '../assets/crew4.png'
+export const CrewData = [
+    {
+        occupation:"Commander",
+        name:"Douglas Hurley",
+        personal_details:"Douglas Gerald Hurley is an American engineer, former  Marine Corps pilot and former NASA astronaunt. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+        image:commander
+    },
+    {
+        occupation:"Mission Specialist",
+        name:"Mark Shuttleworth",
+        personal_details:"Mark Shuttle is the founder and the CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+        image:mission_specialist
+    },
+    {
+        occupation:"Pilot",
+        name:"Victor Glover",
+        personal_details:"Pilot on the first operational flight of the SpaceX Crew Dragon to the international space station. Glover is a commander in U.S. Navy where he pilots an F/A-18. He was a crew member of Expedition 64, and served as a station systems flight engineer",
+        image: pilot
+    },
+    {
+        occupation:"Flight Engineer",
+        name:"Anousheh Ansari",
+        personal_details:"Anousheh Ansari is an Iranian American Engineer and the co-founder of Prodea Systems.Ansari was the fourth self-funded tourist, the first self-funded woman to fly to the ISS,and the first Iranian in space.",
+        image:flight_engineer
+    }
+
+]
--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,7 +1,50 @@
-const Crew = () =>{
-    return(
-        <div className="crew">
+import  { CrewData } from '../components/CrewData'
+import { useState } from 'react';
 
+const Crew = () =>{
+    const[current,setCurrent] = useState(0);
+    const length = CrewData.length;
+
+    if(!Array.isArray(CrewData) || CrewData.length <= 0){
+        return null
+    };
+    return( 
+        <div className="crew">        
+            <div className="crew-detail">
+                <div className="crew-description">
+                    <h2>
+                        <span>02</span><span>Meet Your Crew</span>
+                    </h2>
+                    {
+                        CrewData.map((crew,index)=>{
+                            return(
+                                <div className='crew-details' key={index}>
+                                    {current === index &&(
+                                        <>
+                                            <div className='crew-info'>
+                                                <h4 className='occupation'>{crew.occupation}</h4>
+                                                <h3 className='crew-name'>{crew.name}</h3>
+                                                <p className='crew-overview'>{crew.personal_details}</p>
+                                            
+                                            </div>
+                                            <div classname="slider-btn">
+                                                <button className={index === 0 ? "active" : ""} onClick={() => setCurrent(0)}></button>
+                                                <button className={index === 1 ? "active" : ""}  onClick={() => setCurrent(1)}></button>
+                                                <button className={ index=== 2 ? "active" : ""}  onClick={() => setCurrent(2)}></button>
+                                                <button className={index === 3 ?"active" : ""}  onClick={() =>setCurrent(3)}></button>
+                                            </div>
+                                            <img src={crew.image} alt="Douglaus Hurley" />
+                                        </>
+                                    )}
+                                </div>
+                            )
+                        })
+                  
+                    }
+                </div>
+              
+            </div>
+      
         </div>
     );
 };
